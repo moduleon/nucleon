73282ed93a3d2ea9ed9cc4c913433ab7769b1dca
Kévin Marcachi <kevin.marcachi@acseo-conseil.fr>
2018-08-17 08:06:29 +0200

    Adding constructors in main export

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@
  */
 module.exports = {
     channel: require('@/components/channel/Channel'),
+    Collection: require('@/components/model/classes/Collection'),
     events: require('@/components/event/Events'),
     http: require('@/components/http/Client'),
     memory: require('@/components/browser/Memory'),
+    Model: require('@/components/model/classes/Model'),
     models: require('@/components/model/Models'),
+    Page: require('@/components/page/classes/Page'),
     pages: require('@/components/page/Pages'),
+    View: require('@/components/view/classes/View'),
     views: require('@/components/view/Views'),
     redirect: require('@/components/http/Redirect'),
     router: require('@/components/routing/Router'),
